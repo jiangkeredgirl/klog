798a8e6cb8ed057fb8f83766eadbaf69c4f311a1
jiangke <jiangkeredgirl@hotmail.com>
2017-12-23 15:20:00 +0800

    创建日志工程

---
+++ kloglibtest/kloglibtest.cpp
@@ -0,0 +1,14 @@
+// kloglibtest.cpp : Defines the entry point for the console application.
+//
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+
+int main()
+{
+    return 0;
+}
+
