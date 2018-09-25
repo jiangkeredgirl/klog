0a9df472df162d579b24531bc7cd64f36560bba7
jiangke <jiangkeredgirl@hotmail.com>
2018-09-25 16:23:04 +0800

    增加log对klogclient项目

--- klogclient/main.cpp
+++ klogclient/main.cpp
@@ -1,10 +1,15 @@
 ﻿#include "klogclient.h"
 #include <QtWidgets/QApplication>
+#include "kloglib.h"
 
 int main(int argc, char *argv[])
 {
+	CreateConsole;
+	GenerateDump;
+	TrackCout;	
 	QApplication a(argc, argv);
 	KlogClient w;
 	w.show();
 	return a.exec();
+	WaitTrace;
 }
