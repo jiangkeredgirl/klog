5106b0ba0c78d85fd5299e1d9e910c472f4ff9b4
jiangke <jiangkeredgirl@hotmail.com>
2018-10-18 00:36:17 +0800

    修改编译bat

--- kloglibtest/PostBuildEvent.bat
+++ kloglibtest/PostBuildEvent.bat
@@ -1,7 +1,7 @@
 echo copy bin file of sdk to bin
 echo f|xcopy /y /s /i /f .\KlogSDK\bin\*      .\bin\
-echo f|xcopy /y  .\bin\*.exe                  .\OutKloglibTestBin\
-echo f|xcopy /y  .\bin\*.pdb                  .\OutKloglibTestBin\
-echo f|xcopy /y  .\bin\*.dll                  .\OutKloglibTestBin\
-echo f|xcopy /y  .\bin\*.config               .\OutKloglibTestBin\
+echo f|xcopy /y  .\bin\*.exe                  .\OutKlogLibTestBin\
+echo f|xcopy /y  .\bin\*.pdb                  .\OutKlogLibTestBin\
+echo f|xcopy /y  .\bin\*.dll                  .\OutKlogLibTestBin\
+echo f|xcopy /y  .\bin\*.config               .\OutKlogLibTestBin\
 
