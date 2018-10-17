7052f72706b8ebf380f3b62ee880eb12ca2c7cd1
jiangke <jiangkeredgirl@hotmail.com>
2018-10-17 22:14:37 +0800

    增加工程klogserver

---
+++ klogserver/klogserver.cpp
@@ -0,0 +1,14 @@
+﻿// klogserver.cpp : Defines the entry point for the console application.
+//
+
+//#include "targetver.h"
+//
+//#include <stdio.h>
+//#include <tchar.h>
+
+
+int main()
+{
+    return 0;
+}
+
