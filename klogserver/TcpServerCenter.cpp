61dfa918d1e733fb7507b2c1a30570ee99bdf79c
jiangke <jiangkeredgirl@hotmail.com>
2018-10-18 23:15:04 +0800

    klog server 开发中

--- klogserver/TcpServerCenter.cpp
+++ klogserver/TcpServerCenter.cpp
@@ -21,53 +21,58 @@ TcpServerCenter& TcpServerCenter::instance()
 
 int TcpServerCenter::Run(bool async)
 {
-	cout << "please input server port, such as:9000" << endl;
+	cout << "please input server port, default is 2012" << endl;
 	string strport;
 	getline(std::cin, strport);
-	int port = 9000;
+	int port = 2012;
 	if (!strport.empty())
 	{
 		port = stoi(strport);
 	}
-
-	ITcpServer* tcpserver = TcpLibrary::instance()->NewTcpServer(port);
-	TcpServerHandlerCenter handler_center(tcpserver);
-	tcpserver->RegisterHandler(&handler_center);
-	
-	if (async)
-	{
-		tcpserver->AsyncStart();
-		cout << "tcp async server runing, port:" << port << endl;
-	}
-	else
-	{
-		tcpserver->Start();
-		cout << "tcp sync server runing, port:" << port << endl;
-	}
-	string input_flag;
 	do
 	{
-		cout << "please input text for send to server, \'c\' will close server" << endl;
-		cin >> input_flag;
-		if (input_flag == "c")
+		ITcpServer* tcpserver = TcpLibrary::instance()->NewTcpServer(port);
+		if (tcpserver == nullptr)
 		{
+			cout << "tcp server create failed" << endl;
 			break;
 		}
+		TcpServerHandlerCenter handler_center(tcpserver);
+		tcpserver->RegisterHandler(&handler_center);
 		if (async)
 		{
-			tcpserver->AsyncBroadcast(input_flag.c_str(), input_flag.size());
+			tcpserver->AsyncStart();
+			cout << "tcp async server runing, port:" << port << endl;
 		}
 		else
 		{
-			tcpserver->Broadcast(input_flag.c_str(), input_flag.size());
+			tcpserver->Start();
+			cout << "tcp sync server runing, port:" << port << endl;
 		}
-	} while (true);
-	tcpserver->Stop();
-	cout << "tcp have closed" << endl;
-	if (tcpserver)
-	{
-		TcpLibrary::instance()->DeleteTcpServer(tcpserver);
-	}
-	system("pause");
+		string input_flag;
+		do
+		{
+			cout << "please input text for send to server, \'c\' will close server" << endl;
+			cin >> input_flag;
+			if (input_flag == "c")
+			{
+				break;
+			}
+			if (async)
+			{
+				tcpserver->AsyncBroadcast(input_flag.c_str(), input_flag.size());
+			}
+			else
+			{
+				tcpserver->Broadcast(input_flag.c_str(), input_flag.size());
+			}
+		} while (true);
+		tcpserver->Stop();
+		cout << "tcp have closed" << endl;
+		if (tcpserver)
+		{
+			TcpLibrary::instance()->DeleteTcpServer(tcpserver);
+		}
+	} while (false);
 	return 0;
 }
--- klogserver/klogserver.cpp
+++ klogserver/klogserver.cpp
@@ -5,10 +5,15 @@
 //
 //#include <stdio.h>
 //#include <tchar.h>
-
+#include "cstandard.h"
+#include "TcpServerCenter.h"
 
 int main()
 {
+	cout << "klog server start" << endl;
+	TcpServerCenter::instance().Run(true);	
+	cout << "klog server end" << endl;
+	//system("pause");
     return 0;
 }
 
