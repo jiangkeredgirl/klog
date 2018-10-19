bd43e8332769221a2ba874f48f705023067b1c38
jiangke <jiangkeredgirl@hotmail.com>
2018-10-19 21:01:40 +0800

    调整 networksdk目录位置

--- klogserver/PostBuildEvent.bat
+++ klogserver/PostBuildEvent.bat
@@ -1,5 +1,5 @@
 echo copy bin file of sdk to bin
-echo f|xcopy /y /s /i /f .\NetworkSDK\bin\*      .\bin\
+echo f|xcopy /y /s /i /f ..\NetworkSDK\bin\*     .\bin\
 echo f|xcopy /y  .\bin\*.exe                     .\OutKlogServerBin\
 echo f|xcopy /y  .\bin\*.pdb                     .\OutKlogServerBin\
 echo f|xcopy /y  .\bin\*.dll                     .\OutKlogServerBin\
