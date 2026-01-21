f88d9dd5764a511a023b9575d09099e37a3d8c05
jiangke <jiangkeredgirl@hotmail.com>
2026-01-21 11:03:13 +0800

    变更依赖库boost的lib库目录，需要另外下载boost库，对boost库升级以支持vs2022

--- klognetprotocollib/PostBuildEvent.bat
+++ klognetprotocollib/PostBuildEvent.bat
@@ -1,9 +1,9 @@
 echo copy to out sdk package
 set toolset=%1
-echo f|xcopy /y  .\klognetprotocol.h                             .\OutKlogNetProtocolSDK\include\
-echo f|xcopy /y  .\protocolserial.h                              .\OutKlogNetProtocolSDK\include\
-echo f|xcopy /y  .\protocolserialpackage.h                       .\OutKlogNetProtocolSDK\include\
-echo f|xcopy /y  .\bin\klognetprotocollib*.lib                   .\OutKlogNetProtocolSDK\lib\
-echo f|xcopy /y  .\bin\klognetprotocollib*.dll                   .\OutKlogNetProtocolSDK\bin\
-echo f|xcopy /y  .\bin\klognetprotocollib*.pdb                   .\OutKlogNetProtocolSDK\bin\
-echo f|xcopy /y  ..\..\thirdparty\boost\lib\msvc-%toolset%\boost_serialization*.dll           .\OutKlogNetProtocolSDK\bin\
+echo f|xcopy /y  .\klognetprotocol.h                                                          .\OutKlogNetProtocolSDK\include\
+echo f|xcopy /y  .\protocolserial.h                                                           .\OutKlogNetProtocolSDK\include\
+echo f|xcopy /y  .\protocolserialpackage.h                                                    .\OutKlogNetProtocolSDK\include\
+echo f|xcopy /y  .\bin\klognetprotocollib*.lib                                                .\OutKlogNetProtocolSDK\lib\
+echo f|xcopy /y  .\bin\klognetprotocollib*.dll                                                .\OutKlogNetProtocolSDK\bin\
+echo f|xcopy /y  .\bin\klognetprotocollib*.pdb                                                .\OutKlogNetProtocolSDK\bin\
+echo f|xcopy /y  ..\..\thirdparty\boost\lib64-msvc-%toolset%\boost_serialization*.dll         .\OutKlogNetProtocolSDK\bin\
